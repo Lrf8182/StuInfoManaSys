467d344d8c2e5a468ac0f9482a054f232ad38d0b
JamesNULLiu <jamesnulliu@outlook.com>
2022-07-09 21:31:52 +0800

    many

--- source/.main.cpp
+++ source/.main.cpp
@@ -5,11 +5,15 @@
 #include<process.h>
 #include "../header/ConsoleSetting.h"
 #include <string>
+#include <fstream>
 
 SIMS stuInfo;
 
 int main()
 {
+    std::ofstream myoutput;
+    myoutput.open("aaa.txt");
+
     while (welcome()) {
         system("cls");
     };
@@ -19,7 +23,30 @@ int main()
     colorfulOutput(" ", 1, 6);
     colorfulOutput("lrf\n", 2, 4);
 
+    myoutput << "lrf love me.\n" << std::endl << "!!!";
+
     setOutputColor(4);
     std::cout << "Program ended. Press ENTER to exit...\n";
+    std::cin.get();
+
+    myoutput.close();
+
+    std::ifstream myinput;
+    myinput.open("aaa.txt");
+
+    while (!myinput.eof()) {
+        std::string str;
+        getline(myinput, str);
+        std::cout << str;
+    }
+
+    while (!myinput.eof()) {
+        char ch;
+        ch = myinput.get();
+        std::cout << ch;
+    }
+
+    myinput.close();
+
     std::cin.get();
 }
